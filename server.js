c935e2b57ce6a1599596bffffc5e2e4f6f5346e5
Lily Bergonzat <lilybergonzat@gmail.com>
2025-07-12 21:58:57 +0200

    Added webserver

---
+++ server.js
@@ -0,0 +1,17 @@
+import { configDotenv } from 'dotenv';
+import express from 'express';
+
+configDotenv({ quiet: true });
+
+const app = express();
+
+app.set('trust proxy', Number(process.env.TRUST_PROXY) ?? 0);
+
+app.get('/', (req, res) => {
+    console.log(`JSON file requested by ${req.ip} | ${req.method} ${req.url}`);
+    res.sendFile('videos.json', { root: '.' });
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Listening on port ${process.env.SERVER_PORT}`);
+})
